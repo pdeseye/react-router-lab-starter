9f7ec5f88f42665f6676759fe6422bf6bc1e7a28
Payal Desai <payal@idesai.com>
2021-12-07 14:20:41 -0500

    completed lab with edits

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Home from "./Home";
-import { Link,Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const App = () => {
     return (
