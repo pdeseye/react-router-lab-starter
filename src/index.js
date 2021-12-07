836fff82a4e3783cfa0a3c1bc131d255ac060990
Payal Desai <payal@idesai.com>
2021-12-07 13:54:07 -0500

    completed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './components/App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import Procedures from './components/Procedures';
+import Contact from './components/Contact';
+import Home from './components/Home';
 import './styles.css';
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route
+          index
+          element={
+            <main style={{ padding: "1rem" }}>
+              <Home/>
+              </main>
+          }
+          />
+      <Route path="procedures" element={<Procedures />}/>
+      <Route path="contact" element={<Contact />}/>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+document.getElementById('root')
+);
